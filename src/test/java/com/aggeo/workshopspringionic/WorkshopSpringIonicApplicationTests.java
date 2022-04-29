fec1c6f439c324c8694e6b3eccc7778ebb9eb7be
Aggeo Ribeiro <aggeodossantos@gmail.com>
2022-04-29 08:54:58 -0300

    Projeto criado

---
+++ src/test/java/com/aggeo/workshopspringionic/WorkshopSpringIonicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aggeo.workshopspringionic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringIonicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
