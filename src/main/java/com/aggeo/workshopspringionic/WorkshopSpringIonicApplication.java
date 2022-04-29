876b0144016944318a5fe28ba729ecc8cd52af93
Aggeo Ribeiro <aggeodossantos@gmail.com>
2022-04-29 16:03:16 -0300

    Criando operacao de instanciação

--- src/main/java/com/aggeo/workshopspringionic/WorkshopSpringIonicApplication.java
+++ src/main/java/com/aggeo/workshopspringionic/WorkshopSpringIonicApplication.java
@@ -1,13 +1,31 @@
 package com.aggeo.workshopspringionic;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class WorkshopSpringIonicApplication {
+import com.aggeo.workshopspringionic.domain.Categoria;
+import com.aggeo.workshopspringionic.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class WorkshopSpringIonicApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(WorkshopSpringIonicApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria (null, "Informática");
+		Categoria cat2 = new Categoria (null, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+	}
 }
