3ccd0869e8b20386506a5f3f52530080a2d726b7
Aggeo Ribeiro <aggeodossantos@gmail.com>
2022-05-03 11:05:08 -0300

    Cliente, TIpoCliente, telefones e enderecos

---
+++ src/main/java/com/aggeo/workshopspringionic/domain/Cliente.java
@@ -0,0 +1,102 @@
+package com.aggeo.workshopspringionic.domain;
+
+import com.aggeo.workshopspringionic.domain.enums.TipoCliente;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.*;
+@Entity
+public class Cliente implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private String email;
+    private String cpfouCpnj;
+    private Integer tipo;
+
+    @OneToMany(mappedBy = "cliente")
+    private List<Endereco> enderecos = new ArrayList<>();
+
+    @ElementCollection
+    @CollectionTable(name = "TELEFONE")
+    private Set<String> telefones = new HashSet<>();
+
+    public Cliente() {
+    }
+
+    public Cliente(Integer id, String nome, String email, String cpfouCpnj, TipoCliente tipo) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.cpfouCpnj = cpfouCpnj;
+        this.tipo = tipo.getCod();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpfouCpnj() {
+        return cpfouCpnj;
+    }
+
+    public void setCpfouCpnj(String cpfouCpnj) {
+        this.cpfouCpnj = cpfouCpnj;
+    }
+
+    public TipoCliente getTipo() {
+        return TipoCliente.toEnum(tipo);
+    }
+
+    public void setTipo(TipoCliente tipo) {
+        this.tipo = tipo.getCod();
+    }
+
+    public List<Endereco> getEnderecos() {
+        return enderecos;
+    }
+
+    public Set<String> getTelefones() {
+        return telefones;
+    }
+
+    public void setTelefones(Set<String> telefones) {
+        this.telefones = telefones;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Cliente)) return false;
+        Cliente cliente = (Cliente) o;
+        return getId().equals(cliente.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
