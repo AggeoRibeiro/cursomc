4b52ba3b1401cdc3bf449c57cd9e9850cf0fbe9a
Aggeo Ribeiro <aggeodossantos@gmail.com>
2022-05-09 17:31:37 -0300

    Validacao customizada : email nao repetido na validacao de cliente

---
+++ src/main/java/com/aggeo/workshopspringionic/domain/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,48 @@
+package com.aggeo.workshopspringionic.domain.services.validation;
+
+import com.aggeo.workshopspringionic.domain.Cliente;
+import com.aggeo.workshopspringionic.dto.ClienteDTO;
+import com.aggeo.workshopspringionic.repositories.ClienteRepository;
+import com.aggeo.workshopspringionic.resources.exception.FieldMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+    @Override
+    public void initialize(ClienteUpdate ann) {
+    }
+
+    @Autowired
+    private HttpServletRequest request;
+
+    @Autowired
+    private ClienteRepository repo;
+
+    @Override
+    public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+
+        Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+        Integer uriId = Integer.parseInt(map.get("id"));
+
+        List<FieldMessage> list = new ArrayList<>();
+
+        Cliente aux = repo.findByEmail(objDto.getEmail());
+        if(aux != null && !aux.getId().equals(uriId)){
+            list.add(new FieldMessage("email", "Email já existente"));
+        }
+        for (FieldMessage e : list) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(e.getMessage())
+                    .addPropertyNode(e.getFieldName()).addConstraintViolation();
+        }
+        return list.isEmpty();
+    }
+}
+
--- src/main/java/com/aggeo/workshopspringionic/dto/ClienteDTO.java
+++ src/main/java/com/aggeo/workshopspringionic/dto/ClienteDTO.java
@@ -1,12 +1,14 @@
 package com.aggeo.workshopspringionic.dto;
 
 import com.aggeo.workshopspringionic.domain.Cliente;
+import com.aggeo.workshopspringionic.domain.services.validation.ClienteUpdate;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
