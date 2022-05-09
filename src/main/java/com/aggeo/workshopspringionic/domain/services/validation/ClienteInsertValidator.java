e58b08844676ac94b4f7c53370fae1831d6b6e51
Aggeo Ribeiro <aggeodossantos@gmail.com>
2022-05-09 09:40:11 -0300

    Validacao customizada de CPF ou CNPJ na insercao de cliente

---
+++ src/main/java/com/aggeo/workshopspringionic/domain/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,38 @@
+package com.aggeo.workshopspringionic.domain.services.validation;
+
+import com.aggeo.workshopspringionic.domain.enums.TipoCliente;
+import com.aggeo.workshopspringionic.domain.services.validation.utils.BR;
+import com.aggeo.workshopspringionic.dto.ClienteNewDTO;
+import com.aggeo.workshopspringionic.resources.exception.FieldMessage;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+    @Override
+    public void initialize(ClienteInsert ann) {
+    }
+
+    @Override
+    public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+        List<FieldMessage> list = new ArrayList<>();
+
+        if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())){
+            list.add(new FieldMessage("cpfOuCnpj", "CPF invalido"));
+        }
+
+        if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())){
+            list.add(new FieldMessage("cpfOuCnpj", "CNPJ invalido"));
+        }
+
+        for (FieldMessage e : list) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(e.getMessage())
+                    .addPropertyNode(e.getFieldName()).addConstraintViolation();
+        }
+        return list.isEmpty();
+    }
+}
+
